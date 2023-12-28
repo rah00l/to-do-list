bdb330954d4262adff7520931907903632cce65e
rahul patil <rahulpatil2387@gmail.com>
2023-12-28 21:25:22 +0530

    Update JS and bit HTML to add Delete Task list functionality

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-// Function to add task
-function addTask() {
-
-	// Get the input value
+document.addEventListener('DOMContentLoaded', function() {
+	// Select elements 
 	const taskInput = document.getElementById('taskInput');
-	const inputText = taskInput.value;
+	const addTaskBtn = document.getElementById('addTaskBtn');
+	const taskList = document.getElementById('taskList');
 
-	// Check if input is not empty
-	if (inputText.trim() !== '') {
+	// Function to add task
+	function addTask() {
 
-		// Create a new list item
+		// Get the input value
+		const taskText = taskInput.value.trim();
+
+		// Check if input is not empty
+		if (taskText !== '') {
+			
+			// Create a new list item
+			const listItem = createTaskItem(taskText);
+			taskList.appendChild(listItem);
+			taskInput.value = '';	// Clear the input
+
+		}
+		else{
+			alert("Please enter task!");
+		}
+	}
+	
+	// Create task list item
+	function createTaskItem(taskText) {
+
 		const listItem = document.createElement('li');
-		listItem.textContent = inputText;
+		listItem.textContent = taskText;
 
-		const taskList = document.getElementById('taskList');
-		taskList.appendChild(listItem);
+		
+		// Add a delete button to the list item
+		const deleteBtn = document.createElement('button');
+		deleteBtn.textContent = 'Delete';
+		deleteBtn.ClassName = 'deleteBtn';
+		listItem.appendChild(deleteBtn);
 
-		// Clear the input
-		taskInput.value = '';
+		// Attach a click event listner to the delete button
+		deleteBtn.addEventListener('click', function() {
+			// Call a funtion to handle task delete
+			deleteTask(listItem);
+		})
+
+		return listItem;
 	}
-	else{
-		alert("Please enter task!");
+
+	function deleteTask(taskItem) {
+		// Remove the task item from task list
+		taskList.removeChild(taskItem);
 	}
-}
+
+	// Attach a click event listner to the "Add Task" button
+	addTaskBtn.addEventListener('click', addTask);
+
+})
